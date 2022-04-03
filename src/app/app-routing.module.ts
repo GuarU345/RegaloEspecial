5d3375d59feb92e2f923184f0f5773052c57cdef
Guaru34 <86795974+Guaru34@users.noreply.github.com>
2022-04-03 00:44:32 -0600

    cambio drastico

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { CancionfavComponent } from './Componentes/cancionfav/cancionfav.component';
 import { ContinuarComponent } from './Componentes/continuar/continuar.component';
+import { DashboardComponent } from './Componentes/dashboard/dashboard.component';
 import { Fiesta18Component } from './Componentes/fiesta18/fiesta18.component';
+import { FotofavComponent } from './Componentes/fotofav/fotofav.component';
 import { InicioComponent } from './Componentes/inicio/inicio.component';
+import { PortadaComponent } from './Componentes/portada/portada.component';
+import { PrimerasfotosComponent } from './Componentes/primerasfotos/primerasfotos.component';
+import { PrimermensajeComponent } from './Componentes/primermensaje/primermensaje.component';
 
 const routes: Routes = [
   {path: '', redirectTo:'11/04', pathMatch:'full'},
-  {path:'11/04',component:InicioComponent},
+  {path:'11/04',component:PortadaComponent},
   {path:'fiesta',component:Fiesta18Component},
-  {path:'inicio',component:ContinuarComponent}
+  {path:'inicio',component:ContinuarComponent},
+  {path:'primermensaje',component:PrimermensajeComponent},
+  {path:'primerasfotos',component:PrimerasfotosComponent},
+  {path:'cancion',component:CancionfavComponent},
+  {path:'fotofav',component:FotofavComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,25 @@ import { AppComponent } from './app.component';
 import { InicioComponent } from './Componentes/inicio/inicio.component';
 import { ContinuarComponent } from './Componentes/continuar/continuar.component';
 import { Fiesta18Component } from './Componentes/fiesta18/fiesta18.component';
+import { DashboardComponent } from './Componentes/dashboard/dashboard.component';
+import { PrimermensajeComponent } from './Componentes/primermensaje/primermensaje.component';
+import { PortadaComponent } from './Componentes/portada/portada.component';
+import { PrimerasfotosComponent } from './Componentes/primerasfotos/primerasfotos.component';
+import { CancionfavComponent } from './Componentes/cancionfav/cancionfav.component';
+import { FotofavComponent } from './Componentes/fotofav/fotofav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     InicioComponent,
     ContinuarComponent,
-    Fiesta18Component
+    Fiesta18Component,
+    DashboardComponent,
+    PrimermensajeComponent,
+    PortadaComponent,
+    PrimerasfotosComponent,
+    CancionfavComponent,
+    FotofavComponent
   ],
   imports: [
     BrowserModule,
